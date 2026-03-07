28083c84c0ed6321bdff7abafd22e7f607ebd520
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-07 22:59:12 +0800

    Update app.js
    Rev 25

--- js/app.js
+++ js/app.js
@@ -14,51 +14,6 @@ renderChannels(channels)
 
 })
 
-function renderChannels(list){
-
-container.innerHTML=""
-
-list.forEach(ch=>{
-
-const card=document.createElement("div")
-
-card.className="channel"
-
-card.innerHTML=`
-
-<div class="logo">📻</div>
-
-<h3>${ch.name}</h3>
-
-<p>${ch.artist}</p>
-
-`
-
-card.onclick=()=>playChannel(ch,card)
-
-container.appendChild(card)
-
-})
-
-}
-
-const container=document.getElementById("channels")
-
-let channels=[]
-
-fetch(API_CHANNELS)
-
-.then(res=>res.json())
-
-.then(data=>{
-
-channels=data.channels
-
-renderChannels(channels)
-
-})
-
-
 
 function renderChannels(list){
 
@@ -72,31 +27,13 @@ card.className="channel"
 
 card.innerHTML=`
 
-<div class="logo">📻</div>
-
 <h3>${ch.name}</h3>
 
 <p>${ch.artist}</p>
 
-<button class="fav">⭐</button>
-
 `
 
 card.onclick=()=>playChannel(ch,card,index)
-
-
-
-/* FAVORITE */
-
-card.querySelector(".fav").onclick=(e)=>{
-
-e.stopPropagation()
-
-saveFavorite(ch)
-
-}
-
-
 
 container.appendChild(card)
 
