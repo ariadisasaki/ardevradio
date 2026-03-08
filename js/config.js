fe9ef7b6cc2d6592cfceb6815cb92f0faa3b7a13
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-08 19:21:14 +0800

    Update config.js
    Rev 60

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const API_CHANNELS="https://radio.ariadishut.workers.dev/channels"
+const API_CHANNELS="https://ardevradio.ariadishut.workers.dev/channels"
 
-const API_PLAY="https://radio.ariadishut.workers.dev/play"
+const API_PLAY="https://ardevradio.ariadishut.workers.dev/play"
